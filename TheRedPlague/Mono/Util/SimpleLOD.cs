74c21c33a79f4b9ee118bcfe04f1f37b23ca5e37
Lee23 <31892011+LeeTwentyThree@users.noreply.github.com>
2025-05-29 00:43:16 -0400

    Publish SimpleLOD class

---
+++ TheRedPlague/Mono/Util/SimpleLOD.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace TheRedPlague.Mono.Util;
+
+public class SimpleLOD : MonoBehaviour, IScheduledUpdateBehaviour
+{
+    public float unloadDistance;
+    public GameObject toggledObject;
+
+    private float _unloadDistanceSqr;
+
+    private void Start()
+    {
+        _unloadDistanceSqr = unloadDistance * unloadDistance;
+    }
+
+    private void OnEnable()
+    {
+        UpdateSchedulerUtils.Register(this);
+    }
+    
+    private void OnDisable()
+    {
+        UpdateSchedulerUtils.Deregister(this);
+    }
+
+    public string GetProfileTag()
+    {
+        return "TRP:SimpleLOD";
+    }
+
+    public void ScheduledUpdate()
+    {
+        var closestSqrDistance = Mathf.Min(
+            (Player.main.transform.position - transform.position).sqrMagnitude,
+            (MainCamera.camera.transform.position - transform.position).sqrMagnitude);
+        toggledObject.SetActive(closestSqrDistance < _unloadDistanceSqr);
+    }
+
+    public int scheduledUpdateIndex { get; set; }
+}
