ff9c4d39590ed78e6d32e9cef630e5515dad871b
Lee23 <31892011+LeeTwentyThree@users.noreply.github.com>
2025-05-31 01:08:26 -0400

    Publish WarnForFailedLoadPatcher

---
+++ TheRedPlague/Patches/ErrorHandling/WarnForFailedLoadPatcher.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace TheRedPlague.Patches.ErrorHandling;
+
+[HarmonyPatch]
+public static class WarnForFailedLoadPatcher
+{
+    [HarmonyPatch(typeof(Player), nameof(Player.Start))]
+    [HarmonyPrefix]
+    public static void PlayerPrefix()
+    {
+        if (!Plugin.RedPlagueLoaded)
+        {
+            new GameObject("TRP-Warner").AddComponent<AnnoyingWarningClass>();
+        }
+    }
+
+    private class AnnoyingWarningClass : MonoBehaviour
+    {
+        private void Start()
+        {
+            InvokeRepeating(nameof(Warn), 1, 5);
+        }
+
+        private void Warn()
+        {
+            ErrorMessage.AddMessage("<color=#ff0000>Red Plague failed to load!</color>");
+            Plugin.Logger.LogError("Warning user because the Red Plague failed to load!");
+        }
+    }
+}
