749a8c453a84fce56725ca2a73b12a061f7483ae
Lee23 <31892011+LeeTwentyThree@users.noreply.github.com>
2025-05-31 23:07:24 -0400

    Publish a tiny file that nobody will care about

---
+++ TheRedPlague/Mono/VFX/ConstantRotation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace TheRedPlague.Mono.VFX;
+
+public class ConstantRotation : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 eulerRotation;
+    public Space space;
+
+    private void Update()
+    {
+        target.Rotate(eulerRotation * Time.deltaTime, space);
+    }
+}
