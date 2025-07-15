e77cf28b27864a0d2b934824685b6a7e2fffbeb3
Lee23 <31892011+LeeTwentyThree@users.noreply.github.com>
2025-07-15 19:27:33 -0400

    Create VectorPropertyModifier.cs

---
+++ TheRedPlague/MaterialModifiers/VectorPropertyModifier.cs
@@ -0,0 +1,27 @@
+﻿using Nautilus.Utility;
+using Nautilus.Utility.MaterialModifiers;
+using UnityEngine;
+
+namespace TheRedPlague.MaterialModifiers;
+
+public class VectorPropertyModifier : MaterialModifier
+{
+    private readonly int _id;
+    private readonly Vector4 _value;
+
+    public VectorPropertyModifier(string propertyName, Vector4 value) : this(Shader.PropertyToID(propertyName), value)
+    {
+    }
+
+    public VectorPropertyModifier(int id, Vector4 value)
+    {
+        _id = id;
+        _value = value;
+    }
+
+    public override void EditMaterial(Material material, Renderer renderer, int materialIndex,
+        MaterialUtils.MaterialType materialType)
+    {
+        material.SetVector(_id, _value);
+    }
+}
